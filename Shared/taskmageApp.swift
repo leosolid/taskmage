856cdbd437188f8f412638846220747b20102199
Leo S <13348080+leosolid@users.noreply.github.com>
2021-07-19 19:43:16 -0700

    Initial Commit

---
+++ Shared/taskmageApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmageApp.swift
+//  Shared
+//
+//  Created by Leo Sol on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct taskmageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
